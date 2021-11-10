59138c9be18cae5580c95a67176457e92b43d1aa
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-10 15:28:27 +0600

    Created Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home/Home';
+import Header from './pages/Shared/Header/Header';
+import Footer from './pages/Shared/Footer/Footer';
 
 function App() {
   return (
     <>
-      
+      <Router>
+        <Header />
+        <Switch>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+        </Switch>
+        <Footer />
+      </Router>
     </>
   );
 }
