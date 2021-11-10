58bf580eadfc0347e4702e9693d357195154a369
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-10 15:27:48 +0600

    Added Navbar

---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from '../../../logo.svg';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light" variant="light">
+                <Container>
+                <Navbar.Brand as={NavLink} exact to="/">
+                    <img
+                        alt=""
+                        src={logo}
+                        width="120"
+                        height="50"
+                        className="d-inline-block align-top"
+                        />{' '}
+                </Navbar.Brand>
+                <Nav className="ms-auto">
+                <Nav.Link as={NavLink} exact to="/">Home</Nav.Link>
+                <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Header;
