ae19bcdc033734519a745c818103e9f6ed37b84e
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-10 21:11:27 +0600

    Made Responsive

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -6,23 +6,27 @@ import { NavLink } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <Navbar bg="light" variant="light">
+            <Navbar collapseOnSelect expand="md" bg="light" variant="light">
                 <Container>
-                <Navbar.Brand as={NavLink} exact to="/">
-                    <img
-                        alt=""
-                        src={logo}
-                        width="120"
-                        height="50"
-                        className="d-inline-block align-top"
-                        />{' '}
-                </Navbar.Brand>
-                <Nav className="ms-auto">
-                <Nav.Link as={NavLink} exact to="/">Home</Nav.Link>
-                <Nav.Link as={NavLink} to="/explore">Explore</Nav.Link>
-                <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
-                <Nav.Link as={NavLink} to="/register">Register</Nav.Link>
-                </Nav>
+                    <Navbar.Brand as={NavLink} exact to="/">
+                        <img
+                            alt=""
+                            src={logo}
+                            width="120"
+                            height="50"
+                            className="d-inline-block align-top"
+                            />{' '}
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={NavLink} exact to="/">Home</Nav.Link>
+                            <Nav.Link as={NavLink} to="/explore">Explore</Nav.Link>
+                            <Nav.Link as={NavLink} to="/login">Login</Nav.Link>
+                            <Nav.Link as={NavLink} to="/register">Register</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </div>
