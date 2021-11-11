b674e973cbbc6fbbf3e62ef5b0847157ac085f41
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-11 22:54:36 +0600

    Added Banner

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 
 const Banner = () => {
     return (
         <div>
-            
+            <Carousel fade controls={false}>
+                <Carousel.Item interval={5000}>
+                    <img
+                    className="d-block w-100"
+                    src="./images/1.jpeg"
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                    className="d-block w-100"
+                    src="./images/1.jpeg"
+                    alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                    className="d-block w-100"
+                    src="./images/1.jpeg"
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
