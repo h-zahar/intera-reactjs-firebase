743d0d110f9e6b4a467c50059b0aeeac25bb86f3
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-13 03:39:38 +0600

    Set Feedback Not Required

--- src/pages/Dashboard/DashPages/Users/MyReview/MyReview.js
+++ src/pages/Dashboard/DashPages/Users/MyReview/MyReview.js
@@ -28,7 +28,7 @@ const MyReview = () => {
                 <form style={{maxWidth: '400px', minWidth: '250px'}} className="form-sizing d-flex flex-column py-4 px-5 mx-auto shadow-lg rounded" onSubmit={handleSubmit(onSubmit)}>
                     <h3 className="text-center">Review</h3>
                     <input className="px-2" placeholder="Give Rating (0 - 5)" type="number" {...register("rating", { min: 0, max: 5, required: true })} />
-                    <textarea className="px-2" style={{resize: 'none'}} placeholder="Write Feedback (Max 100 Characters)" {...register("feedback", { maxLength: 100, required: true})}/>
+                    <textarea className="px-2" style={{resize: 'none'}} placeholder="Write Feedback (Max 100 Characters)" {...register("feedback", { maxLength: 100})}/>
                     <input type="submit" />
                 </form>
             </div>
