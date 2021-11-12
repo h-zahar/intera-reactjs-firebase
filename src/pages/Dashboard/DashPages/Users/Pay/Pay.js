0b16fa2b919f82ba194602b5c8ff395ae66befc4
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-13 01:53:58 +0600

    Added Content

--- src/pages/Dashboard/DashPages/Users/Pay/Pay.js
+++ src/pages/Dashboard/DashPages/Users/Pay/Pay.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Pay = () => {
     return (
         <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum accusamus eius eum aspernatur, nostrum alias! Error, deleniti numquam? Quia laborum harum sed quasi delectus aut dolor ipsum atque nobis? Veritatis consequuntur magni illo ex veniam quidem voluptatum itaque debitis quos nihil dignissimos fuga distinctio, molestiae id modi sed. Quisquam, officia?
+            <p className="py-5 text-center fs-5"><strong>Payment System Coming Soon...</strong></p>
         </div>
     )
 }
