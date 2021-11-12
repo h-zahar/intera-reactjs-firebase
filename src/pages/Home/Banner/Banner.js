43b45d69f31128c0a412767d96f39ef798f6f5c6
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-12 07:21:17 +0600

    Polished Banner

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Carousel, Container, Row, Col } from 'react-bootstrap';
+import { Carousel, Container } from 'react-bootstrap';
+import './Banner.css';
 
 const Banner = () => {
     return (
-        <div style={{background: 'rgba(0, 0, 0, 0.75)'}}>
+        <div style={{boxShadow: '-3px -3px 7px rgba(73, 53, 47, 0.699)', background: 'rgba(0, 0, 0, 0.8)'}}>
             <Carousel style={{width: '100%', opacity: '1'}} fade controls={false}>
                 <Carousel.Item interval={8000} style={{transition: 'all 2s ease-in-out', background: `rgba(0, 0, 0, 0.75) url('./images/ban-bg/3.jpg') center center fixed`, backgroundSize: 'cover', backgroundBlendMode: 'darken', borderLeft: '2px solid black', borderRight: '2px solid black'}}>
                     <Container style={{minHeight: '501px'}} className="text-light d-flex justify-content-center align-items-center">
                         <div className="text-center">
-                            <p>Terracotta Lovers are awesome, you know!</p>
-                            <h1>Welcome in your Intera</h1>
+                            <p className="mb-0">Terracotta lovers are awesome, you know!</p>
+                            <h1 className="mt-5 mb-4">Welcome to <span className="brand">Intera</span></h1>
                             <p>Your Empire of Terracotta</p>
                         </div>
                     </Container>
@@ -17,18 +18,18 @@ const Banner = () => {
                 <Carousel.Item interval={8000} style={{transition: 'all 2s ease-in-out', background: `rgba(0, 0, 0, 0.75) url('./images/ban-bg/2.jpg') center center fixed`, backgroundSize: 'cover', backgroundBlendMode: 'darken', borderLeft: '2px solid black', borderRight: '2px solid black'}}>
                     <Container style={{minHeight: '501px'}} className="text-light d-flex justify-content-center align-items-center">
                         <div className="text-center">
-                            <p>Terracotta Lovers are awesome, you know!</p>
-                            <h1>Welcome in your Intera</h1>
-                            <p>Your Empire of Terracotta</p>
+                            <p className="mb-0">Awesome people need awesome place :3</p>
+                            <h1 className="mt-5 mb-4">Welcome to <span className="brand">Intera</span></h1>
+                            <p>Your Palace of Terracotta</p>
                         </div>
                     </Container>
                 </Carousel.Item>
                 <Carousel.Item interval={8000} style={{transition: 'all 2s ease-in-out', background: `rgba(0, 0, 0, 0.75) url('./images/ban-bg/1.jpg') center center fixed`, backgroundSize: 'cover', backgroundBlendMode: 'darken', borderLeft: '2px solid black', borderRight: '2px solid black'}}>
                     <Container style={{minHeight: '501px'}} className="text-light d-flex justify-content-center align-items-center">
                         <div className="text-center">
-                            <p>Terracotta Lovers are awesome, you know!</p>
-                            <h1>Welcome in your Intera</h1>
-                            <p>Your Empire of Terracotta</p>
+                            <p className="mb-0">So, here we are.. For you, by you, with you :D</p>
+                            <h1 className="mt-5 mb-4">Welcome to <span className="brand">Intera</span></h1>
+                            <p>Your Ocean of Terracotta</p>
                         </div>
                     </Container>
                 </Carousel.Item>
