187a94638091568579b5eb50f03888430fed203a
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-13 03:35:12 +0600

    POST Fetch Reviews

--- src/pages/Dashboard/DashPages/Users/MyReview/MyReview.js
+++ src/pages/Dashboard/DashPages/Users/MyReview/MyReview.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
+import useAuth from '../../../../../hooks/useAuth';
 
 const MyReview = () => {
+    const { user } = useAuth();
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+
+    const onSubmit = data => {
+        const postData = {email: user?.email, fullName: user?.displayName, ...data};
+        console.log(postData);
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postData)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data));
+    };
+
     return (
         <div>
             <Container>
