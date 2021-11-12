1af8c84485565a7fef0d6b3ca16810b449158212
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-12 19:31:30 +0600

    Beautified Responsiveness

--- src/pages/Dashboard/DashHeader/DashHeader.js
+++ src/pages/Dashboard/DashHeader/DashHeader.js
@@ -7,16 +7,13 @@ const DashHeader = ({ url }) => {
         <div style={{marginTop: '76.7px'}}>
             <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Toggle className="ms-auto" aria-controls="responsive-navbar-nav" />
 
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mx-auto">
                             <div className="d-flex">
                                 <Nav.Link as={NavLink} exact to={`${url}/orders`}>My Orders</Nav.Link>
                             </div>
-                            {/* <div className="d-flex">
-                                <Nav.Link as={NavLink} to={`${url}/orders`}>My Orders</Nav.Link>
-                            </div> */}
                             <div className="d-flex">
                                 <Nav.Link as={NavLink} to={`${url}/review`}>My Revirew</Nav.Link>
                             </div>
