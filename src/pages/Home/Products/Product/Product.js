1590d54aa8b06f221c8b6f5e2ad9899677af23f7
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2021-11-13 15:40:52 +0600

    Polished

--- src/pages/Home/Products/Product/Product.js
+++ src/pages/Home/Products/Product/Product.js
@@ -9,7 +9,7 @@ const Product = (props) => {
     };
 
     return (
-        <Col sm={6} md={4} lg={3} style={{background: 'rgba(255, 255, 255, 0.4)', borderTop: '1px solid rgba(0, 0, 0, 0.3)', borderBottom: '1px solid rgba(0, 0, 0, 0.3)', borderLeft: '1px solid rgba(0, 0, 0, 0.1)', borderRight: '1px solid rgba(0, 0, 0, 0.1)'}} className="pt-3 gy-4">
+        <Col sm={6} md={4} lg={3} style={{background: 'rgba(255, 255, 255, 0.48)', borderTop: '1px solid rgba(0, 0, 0, 0.3)', borderBottom: '1px solid rgba(0, 0, 0, 0.3)', borderLeft: '1px solid rgba(0, 0, 0, 0.1)', borderRight: '1px solid rgba(0, 0, 0, 0.1)'}} className="pt-3 gy-4">
             <div>
                 <div>
                     <img style={{minHeight: '175px', maxWidth: '100%'}} src={img_i} alt="" />
